480d5805dda8fd797f24676eb915e6518e3683c1
TheSismo <you@example.com>
2024-02-20 11:55:03 +0100

    av

--- Main.java
+++ Main.java
@@ -5,15 +5,13 @@ import java.util.List;
 
 public class Main {
     public static void main() {
-        List<Cliente> listGuests = new LinkedList<Cliente>();
+        List<Cliente> listaClientes = new LinkedList<Cliente>();
+        List<Cliente> Client1 = new Cliente(Guille, 617543067, GuilleSeCagaEnDavidagmail.com);
+        List<Cliente> Client2 = new Cliente(DavidManuelEugenSoos, 643235422, DoboNoTeVeagmail.com);
 
-        List<Habitacion> listRooms = new LinkedList<Habitacion>();
-
-        List<Reserva> listBookings = new LinkedList<Reserva>();
-
-    
+        List<Habitacion> listHabitaciones = new LinkedList<Habitacion>();
+        Habitacion Habitac1 = new Habitacion("Suite", 100, true);
 
     }
-    
     
 }
