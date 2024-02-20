04536bc85b73d3e141e6dafd80c38791c1ea8b30
TheSismo <you@example.com>
2024-02-20 12:40:15 +0100

    Advantage 2.0

--- Main.java
+++ Main.java
@@ -9,7 +9,7 @@ public class Main {
         List<Cliente> Client1 = new Cliente(Guille, 617543067, GuilleSeCagaEnDavidagmail.com);
         List<Cliente> Client2 = new Cliente(DavidManuelEugenSoos, 643235422, DoboNoTeVeagmail.com);
 
-        List<Habitacion> listHabitaciones = new LinkedList<Habitacion>();
+        List<Habitacion> listaHabitaciones = new LinkedList<Habitacion>();
         Habitacion Habitac1 = new Habitacion("Suite", 100, true);
 
     }
--- Reserva.java
+++ Reserva.java
@@ -1,10 +1,20 @@
 package ProyectoHotel;
 
+import java.util.Scanner;
+
 public class Reserva {
 
     String Cliente;
     String Habitacion;
     int DiasDeEstancia;
 
-    public static int PrecioSuite(Suite * DiasDeEstancia)
+    public static void main(String[] args) {
+        int DiasDeEstancia;
+        Scanner leer = new Scanner(System.in);
+
+        System.out.print("Cuantos dias durara tu estancia: ") ;
+        DiasDeEstancia = leer.nextInt();
+
+        System.out.println("El precio de la habitacion es: " + DiasDeEstancia * HabitacionDoble);
+    }
 }
