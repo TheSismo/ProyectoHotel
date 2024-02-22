6c28edc0f9a0686c8be1ae9d3a69b83f5123bfef
TheSismo <you@example.com>
2024-02-22 10:38:24 +0100

    Pressure2

--- Main.java
+++ Main.java
@@ -8,10 +8,14 @@ public class Main {
         List<Cliente> listaClientes = new LinkedList<Cliente>();
         List<Cliente> Client1 = new Cliente(Guille, 617543067, GuilleSeCagaEnDavidagmail.com);
         List<Cliente> Client2 = new Cliente(DavidManuelEugenSoos, 643235422, DoboNoTeVeagmail.com);
+        listaClientes.add(Client1);
+        listaClientes.add(Client2);
 
         List<Habitacion> listaHabitaciones = new LinkedList<Habitacion>();
         List<Habitacion> Habitac1 = new Habitacion("Suite", 100, true);
         List<Habitacion> Habitac2 = new Habitacion("HabitacionIndividual", 22, false);
+        listaHabitaciones.add(Habitac1);
+        listaHabitaciones.add(Habitac2);
         
     }
     
--- Reserva.java
+++ Reserva.java
@@ -7,15 +7,4 @@ public class Reserva {
     String Cliente;
     String Habitacion;
     int DiasDeEstancia;
-
-    public static void main(String[] args) {
-        int DiasDeEstancia;
-        Scanner leer = new Scanner(System.in);
-
-        System.out.print("Cuantos dias durara tu estancia: ") ;
-        DiasDeEstancia = leer.nextInt();
-
-        System.out.println("El precio de la habitacion es: " + DiasDeEstancia * HabitacionDoble);
-    }
-   
 }
