ec4367fd6f99da1dee4c4f126d749094ab578dde
Tu Nombreizanpv <izanpv104@gmail.com>
2024-02-28 12:36:19 +0100

    Archivos movidos

--- Recepcion.java
+++ Recepcion.java
@@ -1,8 +1,12 @@
-package ProyectoHotel;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class Recepcion {
-    List<Cliente> ListaClientes;
+    List<Habitacion> listHabitaciones = new LinkedList<Habitacion>();
+    List<Cliente> listcClientes = new LinkedList<Cliente>();
+    List<Reserva> listReservas = new LinkedList<Reserva>();
 
-    List<Habitacion> ListaHabitaciones;
+    
     
 }
--- Reserva.java
+++ Reserva.java
@@ -1,16 +1,40 @@
-package ProyectoHotel;
 
-import java.util.Scanner;
+
+import ProyectoHotel.Cliente;
 
 public class Reserva {
 
-    String Cliente;
-    String Habitacion;
+    Cliente Cliente;
+    Habitacion Habitacion;
     int DiasDeEstancia;
 
-    public Reserva(String Cliente, String Habitacion, int DiasDeEstancia) {
-        this.Cliente = Cliente;
-        this.Habitacion = Habitacion;
-        this.DiasDeEstancia = DiasDeEstancia;
+    public int CalcularCosto(Reserva reserva) {
+        /*
+         * Precio individual: 45
+         * Precio Doble: 75
+         * Precio Suite: 225
+         */
+
+       
+        //crear una variable costoPorDia y ponerle el costo por noche de ese tipo de habitación
+        int CostoPorDiaSuite = 225;
+        int CostoPorDiaIndividual = 45;
+        int CostoPorDiaDoble = 75;
+        //multiplicar el costo por noche por la cantidad de dias
+        
+        if(reserva.Habitacion.TipoDeHabitacion.equals("Individual")) {
+            return DiasDeEstancia * CostoPorDiaIndividual;
+        }
+        if(reserva.Habitacion.TipoDeHabitacion.equals("Suite")) {
+            return DiasDeEstancia * CostoPorDiaSuite;
+        }
+        if(reserva.Habitacion.TipoDeHabitacion.equals("Doble")) {
+            return DiasDeEstancia * CostoPorDiaDoble;
+        }
+
+        return 0;
+        
+        
     }
+    
 }
